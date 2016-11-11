c12121d795b96d4f82c17fdeb243594ab3a0d345
jpls <jeanphilippels@gmail.com>
2016-11-11 14:26:17 -0600

    index.php
    Index page display with a button which upon clicked showing the results of the 1000 prime numbers

---
+++ index.php
@@ -0,0 +1,33 @@
+<!---/
+
+***************************************
+* Web Application | Generate the 1000 prime numbers beginning with 2 
+ by Jean-Philippe Lo Siou
+****************************************/
+
+-->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Web Application | Generate the 1000 prime numbers beginning with 2 </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="js/script_prime_.js"></script>
+  </head>
+  <body>
+  <div id="wrapper">
+			<div id="title"> Web Application | Generate the 1000 prime numbers beginning with 2 </div>
+			<div id="detail-wrapper">
+			<div id="header">
+				<span class="get_subt"><a href="#" id="get-data-prime" title="Click Here">Get the 1000 prime numbers</a></span>
+			</div>
+			<div id="container">
+			<div id="show-data"></div>
+			</div>
+			</div>
+			<div id="footer"> Web Apps | Jean-Philippe Lo Siou</div>
+  </div>
+   
+</body>
+</html>
